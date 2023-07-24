b9757e35c240513e6d8c3bfa6d45f3e0f2a24433
waltuh <programming1404@gmail.com>
2023-07-25 07:46:04 +0800

    Initial commit

--- src/js/main.js
+++
@@ -1,6 +0,0 @@
-import "../scss/main.scss";
-import "animate.css";
-import AOS from "aos";
-import "aos/dist/aos.css";
-
-Promise.all([import("./helper/observer")]).then(() => AOS.init());
--- vite.config.js
+++
@@ -1,4 +0,0 @@
-export default {
-  base: "/CLIPBORD/", // Replace '/my-app/' with the base URL of your application
-  // Other configuration options...
-};
